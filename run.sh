02e27d826620a624308148b72ccd07de9409c28b
Kristian Gutic <kristian.gutic@gmail.com>
2023-12-02 00:49:49 +0100

    Updating run script

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 
 echo "Adding some modules"
 
-# module add gcc-10.2
+module add gcc-10.2
 
 
 echo "#################"
@@ -11,7 +11,7 @@ echo "    COMPILING    "
 echo "#################"
 
 ## dont forget to use comiler optimizations (e.g. -O3 or -Ofast)
-# g++ -Wall -std=c++17 -O3 src/main.cpp src/file2.cpp -o network
+g++ -Wall -std=c++17 -O3 src/main.cpp src/NeuralNet.cpp src/Activations.cpp -o net
 
 
 echo "#################"
@@ -21,4 +21,4 @@ echo "#################"
 ## use nice to decrease priority in order to comply with aisa rules
 ## https://www.fi.muni.cz/tech/unix/computation.html.en
 ## especially if you are using multiple cores
-# nice -n 19 ./network
+nice -n 19 ./network
